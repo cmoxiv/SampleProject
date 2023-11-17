7b8f85874c8cefb9d85deba69a316d062cfe8f0f
Mo Hossny <cmoxiv@gmail.com>
2023-11-17 23:40:16 +1100

    This is a test message for a test commit!!

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+import pandas as pd
+import numpy as np
+
+
+class A:
+    def __init__(self):
+        print(12)
+        return
+    pass
+
+
+a = A()
+
+df = pd.DataFrame()
+
+print(df)
+
+np.random.standard_t(.1)
