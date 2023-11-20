49ff3549e09e49316d938d520e9cfcde7fd5013f
Mo Hossny <cmoxiv@gmail.com>
2023-11-20 12:15:27 +1100

    Moving python files

---
+++ python/main.py
@@ -0,0 +1,36 @@
+
+
+import pandas as pd
+import numpy as np
+
+
+class A:
+    def __init__(self):
+        print(12)
+        self.x = 123
+        return
+    pass
+
+
+a = A()
+
+df = pd.DataFrame()
+
+print(df)
+
+np.random.standard_t(.1)
+
+np.random.standard_t(.1)
+np.random.standard_t(.1)
+np.random.standard_t(.1)
+
+
+
+# np.random.
+np.random
+
+np.random.chisquare
+
+df = pd.DataFrame()
+
+
